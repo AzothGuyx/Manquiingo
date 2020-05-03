c9ec0f642a6d7cc42c137475b569395e7f3270df
Valter <zuliani13@hotmail.com>
2020-05-03 00:19:13 -0500

    tests agregados: crear y buscar partida

--- src/test/java/com/proyecto/app/ManquilingoApplicationTests.java
+++ src/test/java/com/proyecto/app/ManquilingoApplicationTests.java
@@ -1,12 +1,45 @@
 package com.proyecto.app;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.proyecto.app.models.dao.PartidaDao;
+import com.proyecto.app.models.documents.Partida;
+
+import reactor.core.publisher.Mono;
 
 
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ManquilingoApplicationTests {
+	
+	@Autowired
+	PartidaDao partidaDao;
+	
+	@Test
+	public void crearPartidaTest() {
+		
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("Carlos");
+		a.add("Maria");
+		a.add("Juan");
+		
+		partidaDao.save(new Partida("2", 10, a, "Sistemas")).subscribe();
+	}
+	
+	@Test
+	public void buscarPartidaTest() {
+		
+		Mono<Partida> partidaBuscada = partidaDao.findById("2");
+		assertEquals(partidaBuscada.block().getTema(), "Sistemas");
+	}
 
 	
 	
