54eba4440711c4b88cdf91d79e0020648948b83c
AzothGuyx <cristianxapata@gmail.com>
2020-05-20 11:07:39 -0500

    ligeros cambios

--- src/main/java/com/proyecto/app/controllers/PartidaRestController.java
+++ src/main/java/com/proyecto/app/controllers/PartidaRestController.java
@@ -3,6 +3,7 @@ package com.proyecto.app.controllers;
 import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.proyecto.app.models.documents.Pregunta;
 import reactor.core.publisher.Flux;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/partidas")
 public class PartidaRestController {
 
